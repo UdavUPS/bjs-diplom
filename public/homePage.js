610e78f7189be98b3b5197fcd45b640790e4bfb4
Udav-94 <ups-fti@yandex.ru>
2023-07-14 19:58:52 +0300

    Реализовано получение информации о пользователе и получение текущего курса валют с выводом в таблицу на стр. homePage.js

--- public/homePage.js
+++ public/homePage.js
@@ -1,2 +1,26 @@
 let logoutButton = new LogoutButton();
 logoutButton.action = () => {ApiConnector.logout(()=>{location.reload()})}
+
+ApiConnector.current(reload => {
+    //console.log(reload.success)
+    if (reload.success) {
+        ProfileWidget.showProfile(reload);
+    }
+});
+
+//console.log(ApiConnector.getStocks('RUB'));
+//ApiConnector.getStocks(reload => console.log(reload));
+
+let ratesBoard = new RatesBoard();
+
+function getExchangeRate() {
+    ApiConnector.getStocks(reload => {
+        if (reload.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(reload.data);
+        }
+    });
+}
+getExchangeRate();
+setInterval(getExchangeRate, 60000);
+
