1b41f0490e92e61431b74118c1a14c32b6cdce91
Udav-94 <ups-fti@yandex.ru>
2023-07-24 10:14:54 +0300

    Исправлена ошибка на стр. loginPage.js, добавлен вывод сообщений о неправильном вводе

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,6 +5,8 @@ userForm.loginFormCallback = data => {
         console.log(response);
         if (response.success) {
             location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
         }
     });
 }
@@ -13,6 +15,8 @@ userForm.registerFormCallback = data => {
     ApiConnector.register(data, response =>{
         if (response.success) {
             location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
         }
     })
 }
